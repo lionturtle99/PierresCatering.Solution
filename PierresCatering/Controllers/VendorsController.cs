9617934d41c952a9a004502734e85ccd6c17ff9d
Liam Eller <liamthelastson@gmail.com>
2022-03-04 15:56:20 -0800

    Add /venders page with active link

--- PierresCatering/Controllers/VendorsController.cs
+++ PierresCatering/Controllers/VendorsController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
 using System.Collections.Generic;
+using System;
 
 namespace Bakery.Controllers
 {
   public class VendorController : Controller
   {
-
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      Console.WriteLine("It made it to VendorController.cs");
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
   }
 }
