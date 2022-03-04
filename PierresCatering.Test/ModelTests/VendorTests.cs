c5f0e27e832f0a507fb733836cce0ba3056b2eca
Liam Eller <liamthelastson@gmail.com>
2022-03-04 09:08:40 -0800

    Add project boilerplate

---
+++ PierresCatering.Test/ModelTests/VendorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+    
+  }
+}
