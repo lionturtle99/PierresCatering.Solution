5a800b417eca184da8829ff32b739477cce3cb84
Liam Eller <liamthelastson@gmail.com>
2022-03-04 09:32:28 -0800

    Get a good fail for class constructors

---
+++ PierresCatering.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order testOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+  }
+}
---
+++ PierresCatering.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor testVendor = new Vendor("test");
+      Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
+  }
+}
