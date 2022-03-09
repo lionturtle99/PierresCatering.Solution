9beaec371053e36282c74a398be413c47aefa4e4
Liam Eller <liamthelastson@gmail.com>
2022-03-08 21:46:19 -0800

    Add New.cshtl for adding new orders to a vendor and orderConstroller

---
+++ PierresCatering/Controllers/OrdersConstroller.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+
+namespace Bakery.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+  }
+}
