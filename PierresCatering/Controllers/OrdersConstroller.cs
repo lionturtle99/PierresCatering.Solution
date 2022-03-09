ddc295ed75a0be61561ab94a408237620b89b5fc
Liam Eller <liamthelastson@gmail.com>
2022-03-08 23:14:42 -0800

    Add deleteAll file with coresponding Post method

--- PierresCatering/Controllers/OrdersConstroller.cs
+++ PierresCatering/Controllers/OrdersConstroller.cs
@@ -24,5 +24,12 @@ namespace Bakery.Controllers
       model.Add("vendor", vendor);
       return View(model);
     }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
   }
 }
